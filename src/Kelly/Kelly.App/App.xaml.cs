69950bfee5de4f6fd5d786a1274d532217e38ebf
Andreas Hammar <andreas.hammar@jayway.com>
2012-09-12 11:21:36 +0200

    copy of mainpage to keep a history page with back button

--- src/Kelly/Kelly.App/App.xaml.cs
+++ src/Kelly/Kelly.App/App.xaml.cs
@@ -1,20 +1,10 @@
 ﻿using Kelly.App.Common;
 
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Grid App template is documented at http://go.microsoft.com/fwlink/?LinkId=234226
 
@@ -77,7 +67,7 @@ namespace Kelly.App
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(GroupedItemsPage), "AllGroups"))
+                if (!rootFrame.Navigate(typeof(MainPage)))
                 {
                     throw new Exception("Failed to create initial page");
                 }
