13a97113c9e0db5ea8b52a079f57de21ea23995d
Andreas Hammar <andreas.hammar@jayway.com>
2012-09-13 12:21:22 +0200

    created appbar button for show history

--- src/Kelly/Kelly.App/MainPage.xaml.cs
+++ src/Kelly/Kelly.App/MainPage.xaml.cs
@@ -38,40 +38,11 @@ namespace Kelly.App
             //this.DefaultViewModel["Groups"] = sampleDataGroups;
         }
 
-        /// <summary>
-        /// Invoked when a group header is clicked.
-        /// </summary>
-        /// <param name="sender">The Button used as a group header for the selected group.</param>
-        /// <param name="e">Event data that describes how the click was initiated.</param>
-        void Header_Click(object sender, RoutedEventArgs e)
-        {
-            // Determine what group the Button instance represents
-            var group = (sender as FrameworkElement).DataContext;
-
-            // Navigate to the appropriate destination page, configuring the new page
-            // by passing required information as a navigation parameter
-            //this.Frame.Navigate(typeof(GroupDetailPage), ((SampleDataGroup)group).UniqueId);
-        }
-
-        /// <summary>
-        /// Invoked when an item within a group is clicked.
-        /// </summary>
-        /// <param name="sender">The GridView (or ListView when the application is snapped)
-        /// displaying the item clicked.</param>
-        /// <param name="e">Event data that describes the item clicked.</param>
-        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            // Navigate to the appropriate destination page, configuring the new page
-            // by passing required information as a navigation parameter
-            //var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
-            //this.Frame.Navigate(typeof(ItemDetailPage), itemId);
-        }
-
         private async void ShowSummaryButton_OnTapped(object sender, TappedRoutedEventArgs e)
         {
             var summaryText = CreateSummaryText();
             var dialog = new MessageDialog(summaryText, Res.Instance.GetString("SummaryTitle"));
-            var result = await dialog.ShowAsync();
+            await dialog.ShowAsync();
         }
 
         public void ShowSettingsPanel()
@@ -111,6 +82,11 @@ namespace Kelly.App
 
                 forEachAction(ctrl);
             }
+        }
+
+        private void ShowHistoryButton_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            
         }
     }
 }
